3894668ab8324c569add096a6ace6555459a3c94
mayandra.vmota <mayandra.vmota@SBC0505221W10-1>
2022-05-10 17:19:34 -0300

    Adicionar arquivos de projeto.

---
+++ nove/View/main.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace nove.View
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTabuada = new System.Windows.Forms.Label();
+            this.tbxNumero = new System.Windows.Forms.TextBox();
+            this.lbxResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTabuada
+            // 
+            this.lblTabuada.AutoSize = true;
+            this.lblTabuada.Font = new System.Drawing.Font("Swis721 Blk BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTabuada.Location = new System.Drawing.Point(300, 47);
+            this.lblTabuada.Name = "lblTabuada";
+            this.lblTabuada.Size = new System.Drawing.Size(143, 32);
+            this.lblTabuada.TabIndex = 0;
+            this.lblTabuada.Text = "Tabuada";
+            // 
+            // tbxNumero
+            // 
+            this.tbxNumero.Location = new System.Drawing.Point(300, 103);
+            this.tbxNumero.Name = "tbxNumero";
+            this.tbxNumero.Size = new System.Drawing.Size(143, 23);
+            this.tbxNumero.TabIndex = 1;
+            // 
+            // lbxResultado
+            // 
+            this.lbxResultado.FormattingEnabled = true;
+            this.lbxResultado.ItemHeight = 15;
+            this.lbxResultado.Location = new System.Drawing.Point(300, 147);
+            this.lbxResultado.Name = "lbxResultado";
+            this.lbxResultado.Size = new System.Drawing.Size(143, 94);
+            this.lbxResultado.TabIndex = 2;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxResultado);
+            this.Controls.Add(this.tbxNumero);
+            this.Controls.Add(this.lblTabuada);
+            this.KeyPreview = true;
+            this.Name = "main";
+            this.Text = "main";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.main_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTabuada;
+        private System.Windows.Forms.TextBox tbxNumero;
+        private System.Windows.Forms.ListBox lbxResultado;
+    }
+}
---
+++ nove/View/main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nove.View
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        private void Calculartabuada() 
+        {
+            int num = 0;
+            num = Convert.ToInt32(tbxNumero.Text);
+
+            for (int i = 0; i<= 10; i++)
+            {
+                lbxResultado.Items.Add(num + " x " +  i +" = " + num*i);
+            }
+            tbxNumero.Focus();
+            tbxNumero.SelectAll();
+        }
+        private void Limpartela()
+        {
+            lbxResultado.Items.Clear();
+            tbxNumero.Clear();
+            tbxNumero.Focus();
+        }
+
+        private void main_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (e.KeyChar == 13)
+            {
+                if (string.IsNullOrWhiteSpace(tbxNumero.Text))
+                {
+                    MessageBox.Show("Digite um Numero");
+                    tbxNumero.Focus();
+                    tbxNumero.SelectAll();
+                    return;
+                }
+            }
+            else
+            {
+                lbxResultado.Items.Clear();
+                Calculartabuada();
+            }
+            
+            if (e.KeyChar == 27)
+            {
+                Limpartela();
+            }
+        }
+    }
+}
